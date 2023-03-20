ba3bb46de96e9f468bf7c266dbf655d8413cd476
Ariel <arielgomezmendez94@gmail.com>
2023-03-20 07:31:35 -0400

    Add the logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import Searcher from "./components/Searcher/Searcher";
 import { Col } from "antd";
 import PokemonList from "./components/PokemonList/PokemonList";
+import logo from "../src/statics/logo.svg";
 
 function App() {
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+        <img src={logo} alt="Pokedux" />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
